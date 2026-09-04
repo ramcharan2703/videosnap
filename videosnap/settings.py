c4e0ffc4257497fc69ace241d4fc33c453c8eb67
Ram Charan <ramcharan0127@example.com>
2026-09-04 14:09:11 +0530

    db changed

--- videosnap/settings.py
+++ videosnap/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'videosnap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.mafpkvtymvhdfiufaedo',
-        'PASSWORD': 'Charan@2703r',
-        'HOST': 'aws-0-ap-northeast-1.pooler.supabase.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
